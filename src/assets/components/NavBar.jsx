822abe99e6fd0bdea16ed4b44a6f829de90016c3
yain09 <yain09@gmail.com>
2024-07-05 14:40:40 -0300

    navbar scroll opacity

--- src/assets/components/NavBar.jsx
+++ src/assets/components/NavBar.jsx
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import "../styles/navBar.scss";
+
 export const NavBar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 400) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className="navBar-container">
+    <div className={`navBar-container ${isScrolled ? "scrolled" : ""}`}>
       <div className="navBar">
-      <div style={{display: "flex", alignItems: "center"}}>
-      <p className="brand">Karma</p>
-      <a href="" className="navBar-a"> Contacto </a>
-      <a href="" className="navBar-a"> Tienda </a>
-      </div>
-      <RxHamburgerMenu  style={{fontSize: "30px", color:"#d40b4e", marginRight:"8px"}}/>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <p className="brand">Karma</p>
+          <a href="" className="navBar-a"> Contacto </a>
+          <a href="" className="navBar-a"> Tienda </a>
+        </div>
+        <RxHamburgerMenu style={{ fontSize: "30px", color: "#d40b4e", marginRight: "8px" }} />
       </div>
     </div>
   );
